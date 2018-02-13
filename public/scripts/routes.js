b9424ee2e36be733f4fcb8fe5245954f9c6bdeda
Daniel Nguyen <danielnguyen93@hotmail.com>
2018-02-12 22:08:52 -0800

    created user_agent object

--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -1,7 +1,22 @@
-var routes = function(app) {
+var routes = function(app, userAgent) {
 
   app.get('/', function(req, res) {
-    res.render('index.html');
+
+    var user_agent = {};
+    // create user agent instance to allow output of client information
+    var agent = userAgent.parse(req.headers['user-agent']);
+
+    // User Story: I can get the IP address, language and operating system for my browser.
+    var ip = req.connection.remoteAddress;
+    var language = req.acceptsLanguages('fr', 'es', 'en');
+    var userOs = agent.os.toString();
+
+    user_agent.ipaddress = ip;
+    user_agent.language = language || 'Language not accepted.';
+    user_agent.software = userOs;
+
+    // console.log(user_agent);
+    res.send(user_agent);
   });
 };
 
